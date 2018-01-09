ca847b96cec69e6beec87adb8626823117066bf0
yxzoro <yangxing20107@gmail.com>
2018-01-09 16:55:15 +0800

    .

--- test_MaterialUI/src/index.js
+++ test_MaterialUI/src/index.js
@@ -2,17 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 
+// mixuse bootstrap grid layout here:
+import './bootstrap/css/bootstrap.css' 
+
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import LinearProgress from 'material-ui/LinearProgress';
 
 const App = () => (
-  <MuiThemeProvider>
-  	<LinearProgress mode="indeterminate" />
-  </MuiThemeProvider>
-);
 
+<div class="container">
+  <div class="row">
+    <div class="col-md-5">
+      <MuiThemeProvider >
+  	    <LinearProgress mode="indeterminate" />
+      </MuiThemeProvider>
+    </div>
+    <div class="col-md-5">
+        <button>haha</button>
+    </div>
+  </div>
 
+  <div class="row">
+  </div>
 
+  <div class="row">
+    <button>haha</button>
+  </div>
+</div>
+
+)
 
 
 

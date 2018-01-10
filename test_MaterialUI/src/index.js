bc1eb53aa2454babd456bb27bee2fe3fec3afd1a
yxzoro <yangxing20107@gmail.com>
2018-01-10 21:55:43 +0800

    。

--- test_MaterialUI/src/index.js
+++ test_MaterialUI/src/index.js
@@ -3,14 +3,45 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css'
 
+//must add material-ui componets inside <MuiThemeProvider> like a container ?! SHIT...
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import LinearProgress from 'material-ui/LinearProgress';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
 
 const App = () => (
 <div class='root_div'>
-      <MuiThemeProvider >
-        <LinearProgress mode="indeterminate" />
-      </MuiThemeProvider>
+<MuiThemeProvider>
+
+ <Table>
+    <TableHeader>
+      <TableRow>
+        <TableHeaderColumn>ID</TableHeaderColumn>
+        <TableHeaderColumn>Name</TableHeaderColumn>
+        <TableHeaderColumn>Status</TableHeaderColumn>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      <TableRow>
+        <TableRowColumn>2</TableRowColumn>
+        <TableRowColumn>Randal White</TableRowColumn>
+        <TableRowColumn>Unemployed</TableRowColumn>
+      </TableRow>
+      <TableRow>
+        <TableRowColumn>3</TableRowColumn>
+        <TableRowColumn>Stephanie Sanders</TableRowColumn>
+        <TableRowColumn>Employed</TableRowColumn>
+      </TableRow>
+    </TableBody>
+  </Table>
+
+</MuiThemeProvider>
+
 </div>
 
 )

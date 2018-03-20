9162f81bb469e87ef09706ece62512df9d49fad0
yxzoro <yangxing20107@gmail.com>
2018-03-20 18:17:05 +0800

    。

--- your_spa/src/index.js
+++ your_spa/src/index.js
@@ -5,48 +5,102 @@ import './index.css'
 
 //must add material-ui componets inside <MuiThemeProvider> like a container ?! SHIT...
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import {
-  Table,
-  TableBody,
-  TableHeader,
-  TableHeaderColumn,
-  TableRow,
-  TableRowColumn,
-} from 'material-ui/Table';
+import TextField from 'material-ui/TextField';
 
-const App = () => (
-<div class='root_div'>
-<MuiThemeProvider>
+class App extends React.Component {
+  constructor(props) { 
+    super(props);     
+    this.state = {messages: ['welcome to chatroom!  -_-']};
+    this.onKeyDown = this.onKeyDown.bind(this);
+    this.fetchdata = this.fetchdata.bind(this);
+  }
 
- <Table>
-    <TableHeader>
-      <TableRow>
-        <TableHeaderColumn>ID</TableHeaderColumn>
-        <TableHeaderColumn>Name</TableHeaderColumn>
-        <TableHeaderColumn>Status</TableHeaderColumn>
-      </TableRow>
-    </TableHeader>
-    <TableBody>
-      <TableRow>
-        <TableRowColumn>2</TableRowColumn>
-        <TableRowColumn>Randal White</TableRowColumn>
-        <TableRowColumn>Unemployed</TableRowColumn>
-      </TableRow>
-      <TableRow>
-        <TableRowColumn>3</TableRowColumn>
-        <TableRowColumn>Stephanie Sanders</TableRowColumn>
-        <TableRowColumn>Employed</TableRowColumn>
-      </TableRow>
-    </TableBody>
-  </Table>
+  fetchdata() {
+    var url = "http://139.196.99.113:80/get_messages?last_time=" + (new Date()).valueOf();
+    fetch(url, {
+        method: "get",
+        headers: {"Connection": "keep-alive"},
+    }).then( (response) => {
+          if (response.status == 200){
+            console.log('success');
+          }
+          else{
+            console.log('fail');
+          }
+          return response.text();
+      }).then( (data) => {
+          console.log("server data: " + data); 
+          if (this.state.messages.length >= 16){
+            this.state.messages.shift();  
+          }
+          // data = "[u'hi', u'hello']"
+          data = data.replace(/u'/g, "'");
+          this.setState({messages: this.state.messages.concat( eval(data) )});
+          setTimeout(() => this.fetchdata(), 1000);
+      }).catch( (err) => {
+          console.log(err);
+          setTimeout(() => this.fetchdata(), 1000);
+      });
+  }
 
-</MuiThemeProvider>
+  postdata(message) {
+    var url = "http://139.196.99.113:80/post_messages?last_time=" + (new Date()).valueOf() + "&&message=" + message;
+    fetch(url, {
+        method:"get",
+    }).then( (response) => {
+          if (response.status == 200){
+            console.log('post data success');
+          }
+          else{
+            console.log('post data fail');
+          }
+      }).catch( (err) => {
+          console.log(err);
+      });
+  }
 
-</div>
+  componentDidMount() {
+    this.fetchdata();
+  }
 
-)
+  onKeyDown(e){
+    if(e.keyCode == 13){
+      if (this.state.messages.length >= 16){
+        this.state.messages.shift();  
+      }
+      this.setState({messages: this.state.messages.concat(e.target.value)});
+      this.postdata(e.target.value);
+      e.target.value = null;
+      }
+    }
 
+  render(){ 
+    const Items = this.state.messages.map((number) =>
+      <li >{number}</li> 
+    );
+    return (
+      <div className='root_div'>
+      <MuiThemeProvider>
+      <div className='second_div'>
+
+          <div className='message_field_clss'>
+            <ul>{Items}</ul>
+          </div>
+
+          <TextField className='text_field_clss' onKeyDown={this.onKeyDown}
+            id="your_text"
+            hintText="Text here"
+            fullWidth={true}
+          />
+
+      </div>
+      </MuiThemeProvider>
+      </div>
+    )
+  }
+}
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
